d9e620ecf05b8eab0ab820215a0b5a77bc01e917
Martossy Alex <github@mail.martossy.hu>
2023-07-12 16:27:52 +0200

    app-shell

---
+++ src/app/app-shell/app-shell.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-app-shell',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-shell.component.html',
+  styleUrls: ['./app-shell.component.scss']
+})
+export class AppShellComponent {
+
+}
