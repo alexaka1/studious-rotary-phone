d6507035d8e62e40da07a8eccdea70b3cdd40f79
Martossy Alex <github@mail.martossy.hu>
2023-07-26 17:23:28 +0200

    contentful trial

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Entry } from 'contentful';
+
+// const CONFIG = {
+//   space: process.env.CONTENTFUL_SPACE_ID,
+//   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+//
+//   contentTypeIds: {
+//     blog: 'pageBlogPost',
+//   },
+// };
 
 @Component({
   standalone: true,
@@ -7,4 +17,28 @@ import { CommonModule } from '@angular/common';
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss'],
 })
-export class BlogComponent {}
+export class BlogComponent /*implements OnInit*/ {
+  // private cdaClient = createClient({
+  //   space: CONFIG.space,
+  //   accessToken: CONFIG.accessToken,
+  // });
+  public posts: Entry<any>[] = [];
+
+  constructor() {}
+
+  // async getProducts(query?: object): Promise<Entry[]> {
+  //   const res = await this.cdaClient.getEntries(
+  //     Object.assign(
+  //       {
+  //         content_type: CONFIG.contentTypeIds.blog,
+  //       },
+  //       query
+  //     )
+  //   );
+  //   return res.items;
+  // }
+  //
+  // async ngOnInit(): Promise<void> {
+  //   this.posts = await this.getProducts();
+  // }
+}
