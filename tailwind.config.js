6fcaa8729e45b60c2ed8f191ab40fd33aa94918b
Martossy Alex <github@mail.martossy.hu>
2023-07-26 08:38:46 +0200

    tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ["./src/index.html", "./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
